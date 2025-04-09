71f11fe45d5c51a3c0f0f4e1bc6a2534d3262179
othmane <othmanelaafar17@gmail.com>
2025-04-09 19:19:48 +0100

    ajout classe Administrateur, Cours , Enseignant, Etudiant, Salle , Utillisateur

--- src/main/java/gestion/emploi/Main.java
+++ src/main/java/gestion/emploi/Main.java
@@ -2,7 +2,7 @@ package gestion.emploi;
 
 public class Main {
     public static void main(String[] args){
-        Groupe g = new Groupe(1, "3INFO-A");
+        Groupe g = new Groupe();
         Etudiant e = new Etudiant(1, "Sami", "sami@mail.com", g);
     }
 }
